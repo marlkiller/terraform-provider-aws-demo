61c6cf10d791b39f7773a419570272088ac209d6
artemis <marlkiller@voidm.com>
2023-02-06 18:15:22 +0800

    update terraform update lambda

--- test/local_notify_test.py
+++ test/local_notify_test.py
@@ -1,12 +1,9 @@
-import boto3
-import json
 from datetime import datetime
 
+import boto3
 
 client = boto3.client(
     'events',
-    aws_access_key_id='AKIAUK7LDPSVOWZAH57B',
-    aws_secret_access_key='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',
     region_name='cn-north-1'
 )
 
